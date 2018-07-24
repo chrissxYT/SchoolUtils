2b2b5c808b41d06e5b4da15a45ef4df4d5d659e9
chrissx <christian.chrissicx2004@freenet.de>
2018-07-24 18:57:36 +0200

    Added ListPrinters and GetDotNetVersion and removed a few unnecessary imports.

---
+++ GetDotNetVersion/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace GetDotNetVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rk = Convert.ToInt32(RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey("SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\").GetValue("Release"));
+            Console.WriteLine("Version: " + (rk >= 461808 ? "4.7.2 or later" : rk >= 461308 ? "4.7.1 or later" : rk >= 460798 ? "4.7 or later" : rk >= 394802 ? "4.6.2 or later" : rk >= 394254 ? "4.6.1 or later"
+            : rk >= 393295 ? "4.6 or later" : rk >= 393273 ? "4.6 RC or later" : rk >= 379893 ? "4.5.2 or later" : rk >= 378675 ? "4.5.1 or later" : rk >= 378389 ? "4.5 or later" : "No 4.5 or later version detected"));
+        }
+    }
+}
---
+++ ListPrinters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing.Printing;
+using static System.Console;
+
+namespace ListPrinters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string printer in PrinterSettings.InstalledPrinters)
+                WriteLine(printer);
+            ReadLine();
+        }
+    }
+}
